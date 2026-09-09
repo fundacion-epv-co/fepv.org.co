8ce582572bcf3cda445d04956a46908c114ed7ef
fundacion-epv-co <fundacion.epv.co@gmail.com>
2026-09-09 04:28:02 -0500

    Create patchIntranet.js

---
+++ patchIntranet.js
@@ -0,0 +1,5 @@
+﻿const fs = require('fs');
+let file = fs.readFileSync('app/intranet/page.js', 'utf8');
+file = file.replace('setConsEmail(email);', 'setConsEmail(email);\n        setProfileNombre(res.nombre || "");\n        setProfileCargo(res.cargo || "");\n        setProfileDireccion(res.direccion || "");\n        setProfileTelefono(res.telefono || "");');
+fs.writeFileSync('app/intranet/page.js', file, 'utf8');
+console.log('Fixed handleLogin');
